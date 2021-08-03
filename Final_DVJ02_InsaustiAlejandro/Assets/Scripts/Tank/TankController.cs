be59ff3a729ab3f771804ccbdd7f6c0798163053
alejandroninsausti <alejandroninsausti@gmail.com>
2021-08-03 16:45:09 -0300

    Add Tank Movement

---
+++ Final_DVJ02_InsaustiAlejandro/Assets/Scripts/Tank/TankController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+	internal TankData data;
+	[SerializeField] float movementSpeed;
+	[SerializeField] float rotationSpeed;
+	[SerializeField] float headRotationSpeed;
+
+    #region Unity Events
+    private void FixedUpdate()
+    {
+        Move(Input.GetAxis("Vertical"));
+        Rotate(Input.GetAxis("Horizontal"));
+    }
+    #endregion
+
+    #region Methods
+    internal void Move(float forwardMovement)
+    {
+		transform.Translate(Vector3.forward * forwardMovement * movementSpeed);
+    }
+	internal void Rotate(float horizontalMovement)
+    {
+		transform.Rotate(0, horizontalMovement * rotationSpeed, 0);
+    }
+	#endregion
+}
