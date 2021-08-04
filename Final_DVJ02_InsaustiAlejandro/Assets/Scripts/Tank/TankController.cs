d89459809d42f445b5aa41134235b5841359cd8f
alejandroninsausti <alejandroninsausti@gmail.com>
2021-08-03 21:05:02 -0300

    Add Head Rotation

--- Final_DVJ02_InsaustiAlejandro/Assets/Scripts/Tank/TankController.cs
+++ Final_DVJ02_InsaustiAlejandro/Assets/Scripts/Tank/TankController.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
     public Action tankMoved;
-	internal TankData data;
-	[SerializeField] float movementSpeed;
-	[SerializeField] float rotationSpeed;
-	[SerializeField] float headRotationSpeed;
+    internal TankData data;
+    [SerializeField] Transform head;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float headRotationSpeed;
+    float headRotationTimer = 0;
+    RaycastHit cannonTargetHit;
 
     #region Unity Events
     private void FixedUpdate()
     {
         Move(Input.GetAxis("Vertical"));
         Rotate(Input.GetAxis("Horizontal"));
+        if (Input.GetMouseButton(0))
+        {
+            headRotationTimer = 0;
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out cannonTargetHit);
+        }
+        else if (headRotationTimer <= headRotationSpeed)
+        {
+            Shoot(cannonTargetHit.point);
+        }
     }
     #endregion
 
@@ -21,14 +34,22 @@ public class TankController : MonoBehaviour
     internal void Move(float forwardMovement)
     {
         if (Mathf.Abs(forwardMovement) < 0.1f) { return; }
-		transform.Translate(Vector3.forward * forwardMovement * movementSpeed);
+        transform.Translate(Vector3.forward * forwardMovement * movementSpeed);
         tankMoved?.Invoke();
     }
-	internal void Rotate(float horizontalMovement)
+    internal void Rotate(float horizontalMovement)
     {
         if (Mathf.Abs(horizontalMovement) < 0.1f) { return; }
-		transform.Rotate(0, horizontalMovement * rotationSpeed, 0);
+        transform.Rotate(0, horizontalMovement * rotationSpeed, 0);
         tankMoved?.Invoke();
+    }
+    internal void Shoot(Vector3 shootPosition)
+    {
+        shootPosition = new Vector3(shootPosition.x, head.position.y, shootPosition.z);
+        Vector3 shootDirection = shootPosition - head.position;
+        Quaternion toRotation = Quaternion.LookRotation(shootDirection, head.up);
+        headRotationTimer += Time.deltaTime;
+        head.rotation = Quaternion.Lerp(head.rotation, toRotation, headRotationTimer / headRotationSpeed);
     }
     #endregion
 }
